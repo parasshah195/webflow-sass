dbaede71ec928f7d8a7827b77b2c852e459f54ff
Paras Shah <parasshah195@gmail.com>
2023-10-07 17:24:59 +0530

    Prettify Format

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,11 @@
 # 1.1.2
+
 - UI style updates to better match the new Webflow design
 - Fix editor height whenever iframe window resizes
 - Tab indentation in the editor
 
 # 1.1.1
+
 - Finally fix bundle 404 error - add relative path to build config
 
 # 1.1.0
--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,13 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      pages: 'dist',
+      pages: 'dist'
 
       // NOTE: Don't enable `fallback`. Adding a fallback causes relative path to fail, causing Webflow app to not load from within a subdirectory.
-      // fallback: 'index.html' 
+      // fallback: 'index.html'
     }),
     paths: {
-      relative: true,
+      relative: true
     }
   }
 };
