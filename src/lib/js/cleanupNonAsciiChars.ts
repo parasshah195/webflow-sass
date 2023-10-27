f6710185229e833c40b07c74f887c55326b0bf01
Paras Shah <parasshah195@gmail.com>
2023-10-27 20:25:31 +0530

    Silence regex error

--- src/lib/js/cleanupNonAsciiChars.ts
+++ src/lib/js/cleanupNonAsciiChars.ts
@@ -1,4 +1,5 @@
 export function cleanupNonAsciiChars(input: string) {
+  // eslint-disable-next-line no-control-regex
   const nonAsciiChars = /[^\x00-\x7F]/g;
   return input.replace(nonAsciiChars, '');
 }
