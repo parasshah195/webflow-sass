316614af6851b933a52257a369f8aa23745ffb53
Paras Shah <parasshah195@gmail.com>
2023-11-02 17:58:57 +0530

    Update `element` word language to `file for `.scss` and `.css` in app error notices

--- src/lib/js/webflowNotify.ts
+++ src/lib/js/webflowNotify.ts
@@ -22,19 +22,20 @@ export async function showWebflowSuccessfulSave() {
 export const ERROR_TEXTS = {
   emptyFile: "Can't save an empty code file",
   sassDomNotFound:
-    'Unable to locate current .scss element. Is it in a component? \n\nCheck Error notes for next steps',
+    'Unable to locate current .scss file. Is it in a component? \n\nCheck Error notes for next steps',
   sassDomCantUpdate:
-    'Unable to update the current .scss element. \n\nCopy this code and start a new code file',
-  invalidCodeElParent: 'Select a proper element to nest the Sass and compiled CSS code',
-  invalidCssParent: 'Please select the parent element which has the .scss element, then save again',
-  invalidSassElLoad: 'This does not look like a valid .scss code element',
+    'Unable to update the current .scss file. \n\nCopy this code and start a new code file',
+  invalidCodeElParent:
+    "This element can't nest .scss file. Select a valid parent element like `div`",
+  invalidCssParent: 'Please select the parent element which has the .scss file, then save again',
+  invalidSassElLoad: 'This does not look like a valid .scss file',
   sassLoadNoCode: 'No Sass code found on this element',
-  cssElNotFound: 'Unable to find existing .css element',
+  cssElNotFound: 'Unable to find existing .css file',
   missingFilename: 'Please enter a filename'
 };
 
 export const INFO_TEXTS = {
-  cssUpdateError: 'Unable to update CSS element. Creating a new one.'
+  cssUpdateError: 'Unable to update .css file. Creating a new one.'
 };
 
 export async function showCssElNotFoundErrors() {
